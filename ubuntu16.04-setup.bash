45fee3cfc6ca4c8e745c5c8c5e51e2a18743e568
money-gon <ti4042@yahoo.co.jp>
2017-05-20 22:45:59 +0900

    add ros and ubuntu16.04 setup tips

--- ubuntu16.04-setup.bash
+++ ubuntu16.04-setup.bash
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 # manual english to japanese
-sudo apt install language-pack-ja
-sudo update-locale LANG=ja_JP.UTF-8
-sudo apt install manpages-ja
+$ sudo apt install language-pack-ja
+$ sudo update-locale LANG=ja_JP.UTF-8
+$ sudo apt install manpages-ja
+
+# folder japanese to english
+$ LANG=C xdg-user-dirs-gtk-update
+
